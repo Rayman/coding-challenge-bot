b6c6622e1027fb4b19d7d4d5798758d54e5f19dc
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-03-16 13:22:54 +0100

    Write a greedy robot

---
+++ greedy.py
@@ -0,0 +1,46 @@
+from random import choice
+
+import numpy as np
+
+from ..bot_control import Move
+
+
+def determine_new_tile_colour(floor_colour, bot_colour):
+    if floor_colour == 0: return bot_colour  # Bot always paints a white floor tile
+    return [floor_colour, 0, bot_colour][(bot_colour - floor_colour) % 3]
+
+
+class Greedy:
+
+    def __init__(self):
+        self.target = None
+
+    def get_name(self):
+        return "Greedy Gerard"
+
+    def get_contributor(self):
+        return "Rayman"
+
+    def determine_next_move(self, grid, enemies, game_info):
+        moves = {
+            Move.UP: np.array([0, 1], dtype=np.int16),
+            Move.RIGHT: np.array([1, 0], dtype=np.int16),
+            Move.LEFT: np.array([-1, 0], dtype=np.int16),
+            Move.DOWN: np.array([0, -1], dtype=np.int16),
+        }
+
+        moves_with_points = []
+        for value, move in moves.items():
+            target = self.position + move
+            try:
+                color = grid[target[1], target[0]]
+            except IndexError:
+                continue
+            is_points = color != self.id and determine_new_tile_colour(color, self.id) == self.id
+            if is_points:
+                moves_with_points.append(value)
+
+        if moves_with_points:
+            return choice(moves_with_points)
+        else:
+            return choice(list(moves.keys()))
