aa1987c3512f8ffd7d065ad4ee682eb954e5d8a0
Ramon Wijnands <ramon.wijnands@nobleo.nl>
2023-04-20 09:43:28 +0200

    Rename files more logical

--- Makefile
+++ Makefile
@@ -1,24 +1,24 @@
-all: smarter_min.py search_min.py
+all: dfs_min.py astar_min.py
 
-smarter_min.py: smarter.py Makefile
-	pyminify smarter.py --rename-globals --remove-literal-statements --remove-asserts --remove-debug --output smarter_min.py
-	sed -i 's/\bcurrent_score\b/z/g' smarter_min.py
-	sed -i 's/\bdepth\b/y/g' smarter_min.py
-	sed -i 's/\bgrid\b/x/g' smarter_min.py
-	sed -i 's/\bhistory\b/v/g' smarter_min.py
-	sed -i 's/\bmax_depth\b/r/g' smarter_min.py
-	sed -i 's/\bscore_grid\b/s/g' smarter_min.py
+dfs_min.py: dfs.py Makefile
+	pyminify dfs.py --rename-globals --remove-literal-statements --remove-asserts --remove-debug --output dfs_min.py
+	sed -i 's/\bcurrent_score\b/z/g' dfs_min.py
+	sed -i 's/\bdepth\b/y/g' dfs_min.py
+	sed -i 's/\bgrid\b/x/g' dfs_min.py
+	sed -i 's/\bhistory\b/v/g' dfs_min.py
+	sed -i 's/\bmax_depth\b/r/g' dfs_min.py
+	sed -i 's/\bscore_grid\b/s/g' dfs_min.py
 
-search_min.py: search.py Makefile
-	pyminify search.py --rename-globals --remove-literal-statements --remove-asserts --remove-debug --output search_min.py
-	sed -i 's/\bcurrent_score\b/z/g' search_min.py
-	sed -i 's/\bdepth\b/y/g' search_min.py
-	sed -i 's/\bgrid\b/x/g' search_min.py
-	sed -i 's/\bheap\b/w/g' search_min.py
-	sed -i 's/\bhistory\b/v/g' search_min.py
-	sed -i 's/\bmax_depth\b/u/g' search_min.py
-	sed -i 's/\bneighbors\b/t/g' search_min.py
-	sed -i 's/\bpossible_neighbor\b/s/g' search_min.py
-	sed -i 's/\bprevious_positions\b/r/g' search_min.py
-	sed -i 's/\bscore_grid\b/q/g' search_min.py
-	sed -i 's/\bupper_bound\b/p/g' search_min.py
+astar_min.py: astar.py Makefile
+	pyminify astar.py --rename-globals --remove-literal-statements --remove-asserts --remove-debug --output astar_min.py
+	sed -i 's/\bcurrent_score\b/z/g' astar_min.py
+	sed -i 's/\bdepth\b/y/g' astar_min.py
+	sed -i 's/\bgrid\b/x/g' astar_min.py
+	sed -i 's/\bheap\b/w/g' astar_min.py
+	sed -i 's/\bhistory\b/v/g' astar_min.py
+	sed -i 's/\bmax_depth\b/u/g' astar_min.py
+	sed -i 's/\bneighbors\b/t/g' astar_min.py
+	sed -i 's/\bpossible_neighbor\b/s/g' astar_min.py
+	sed -i 's/\bprevious_positions\b/r/g' astar_min.py
+	sed -i 's/\bscore_grid\b/q/g' astar_min.py
+	sed -i 's/\bupper_bound\b/p/g' astar_min.py
--- test_astar.py
+++ test_astar.py
@@ -5,68 +5,65 @@ import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
 
-from robots.rayman.smarter import Move, score_all_moves, direction_to_move, calculate_score_grid
+from robots.rayman.astar import Move, find_best_moves, direction_to_move, calculate_score_grid
 
 
 def test_calculate_score_grid():
     grid = np.array([[0, 1, 2, 3, 4]])
     scores = calculate_score_grid(grid, 4)
 
-    assert scores[0, 0] == 1, 'white'
+    assert scores[0, 0] == 0.9090909090909091, 'white'
     assert scores[0, 1] == 0, "enemy that can't be taken"
-    assert scores[0, 2] == 1 + 1e-6, 'enemy that can be overwritten'
-    assert scores[0, 3] == 1e-6, 'enemy that converts to white'
+    assert scores[0, 2] == 1, 'enemy that can be overwritten, highest score'
+    assert scores[0, 3] == 0.09090909090909091, 'enemy that converts to white'
     assert scores[0, 4] == 0, 'own tile stays the same'
 
 
 def test_out_of_bounds():
     grid = np.array([[4]])
     position = (0, 0)
-    scores = [s for m, s in score_all_moves(grid, position, 0, 4)]
+    scores = [s for m, s in find_best_moves(grid, position, 0, 4)]
     assert all(score == float('-inf') for score in scores)
 
 
 def test_out_of_bounds2():
     grid = np.array([[4, 4]])
     position = (0, 0)
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
-    assert moves_with_scores[Move.LEFT] == float('-inf')
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
+    assert len(moves_with_scores) == 1
     assert moves_with_scores[Move.RIGHT] == 0
-    assert moves_with_scores[Move.UP] == float('-inf')
-    assert moves_with_scores[Move.DOWN] == float('-inf')
 
 
 def test_avoid_own_color():
     grid = np.array([[4, 4],
                      [0, 0]])
     position = (0, 0)
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
-    assert moves_with_scores[Move.RIGHT] == 0
-    assert moves_with_scores[Move.UP] > moves_with_scores[Move.RIGHT]
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
+    assert moves_with_scores[Move.UP] > 0 and len(moves_with_scores) == 1
 
 
 def test_avoid_enemies_that_cant_be_taken():
     grid = np.array([[4, 1],
                      [0, 0]])
     position = (0, 0)
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
-    assert moves_with_scores[Move.UP] > 0 == moves_with_scores[Move.RIGHT]
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
+    assert moves_with_scores[Move.UP] > 0 and len(moves_with_scores) == 1
 
 
 def test_prefer_to_take_enemies():
     grid = np.array([[4, 2],
                      [0, 0]])
     position = (0, 0)
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
-    assert moves_with_scores[Move.RIGHT] > moves_with_scores[Move.UP] > 0
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
+    assert moves_with_scores[Move.RIGHT] > 0 and len(moves_with_scores) == 1
 
 
 def test_prefer_to_convert_to_white():
     grid = np.array([[4, 3],
                      [4, 0]])
     position = (0, 0)
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
-    assert moves_with_scores[Move.RIGHT] > 0 == moves_with_scores[Move.UP]
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
+    assert moves_with_scores[Move.RIGHT] > 0 and len(moves_with_scores) == 1
 
 
 def test_should_not_get_stuck():
@@ -76,12 +73,12 @@ def test_should_not_get_stuck():
     position = np.array([0, 0])
 
     # with a depth of 0, both directions should be equal
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 0, 4)}
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 0, 4)}
     assert moves_with_scores[Move.UP] == moves_with_scores[Move.RIGHT] > 0
 
     # with a depth of 1, the dead end should be seen
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 1, 4)}
-    assert moves_with_scores[Move.UP] > moves_with_scores[Move.RIGHT] > 0
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 1, 4)}
+    assert moves_with_scores[Move.UP] > 0 and len(moves_with_scores) == 1
 
 
 def test_should_not_evaluate_going_back():
@@ -89,8 +86,8 @@ def test_should_not_evaluate_going_back():
                      [0, 4, 4],
                      [0, 0, 4]])
     position = np.array([0, 0])
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 2, 4)}
-    assert moves_with_scores[Move.UP] > moves_with_scores[Move.RIGHT] > 0
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 2, 4)}
+    assert moves_with_scores[Move.UP] > 0 and len(moves_with_scores) == 1
 
 
 def test_can_evaluate_starting_position():
@@ -101,5 +98,5 @@ def test_can_evaluate_starting_position():
     grid = np.array([[0, 0, 0],
                      [0, 4, 4]])
     position = np.array([0, 0])
-    moves_with_scores = {direction_to_move(m): s for m, s in score_all_moves(grid, position, 3, 4)}
-    assert moves_with_scores[Move.UP] > moves_with_scores[Move.RIGHT] > 0
+    moves_with_scores = {direction_to_move(m): s for m, s in find_best_moves(grid, position, 3, 4)}
+    assert moves_with_scores[Move.UP] > 0 and len(moves_with_scores) == 1
